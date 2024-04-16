1754823ba125b0a0b8ffb08dd94ad99952c9a75e
vernonthedev <techjaja2@gmail.com>
2024-04-16 11:33:42 -0600

    Moved my cards array to heap memory

--- cardGame.c
+++ cardGame.c
@@ -15,8 +15,14 @@ int cash = 100;
 // the play function
 void Play(int bet)
 {
-    char C[3] = {'J', 'Q', 'K'}; //define our array of cards
-    printf("Shuffling.....\n");
+    // char C[3] = {'J', 'Q', 'K'}; //define our array of cards
+    // printf("Shuffling.....\n");
+
+    //Now store the characters on the Heap Memory
+    char *C = (char*)malloc(3*sizeof(char)); //c++: char *C = new char[3];
+    C[0] = 'J'; C[1] = 'Q', C[2] = 'K'; //AS A RESULT WE SEE AN INCREMENT IN THE MEM SIZE IN OUR TASK PROCESS MANAGER
+
+
     srand(time(NULL)); //generate random numbers based on time or seeding the random numbers based on time
     int i;
     //shuffle the cards five times
