b6bff88e2e210e928f9e885e3ac16f211e71a5f2
vernonthedev <techjaja2@gmail.com>
2024-04-16 10:55:02 -0600

    initial commit

---
+++ callByReference.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	char C[4];
+	C[0] = 'J';
+	C[1] = 'O';
+	C[2] = 'H';
+	C[3] = 'N';
+	C[4] = '\0';
+	printf("%s", C);
+}
---
+++ cardGame.c
@@ -0,0 +1,4 @@
+//Simple Betting Game
+// "Jack, Queen, King" - computer shuffles these cards
+
+#include <stdio.h>
